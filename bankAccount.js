ce89bcf3e14b010cacdbe9dff0e9535350c0d20d
Denis <deniam24@gmail.com>
2023-09-05 23:00:49 +0100

    added acceptance test criteria and print statement function

--- bankAccount.js
+++ bankAccount.js
@@ -10,16 +10,29 @@ class BankAccount {
 
     makeDeposit(amount, date) {
         this.balance += amount;
-        this.transactions.push( {date: date, credit: amount, debit: '', balance: this.balance});
+        this.transactions.push( {date: date, credit: amount.toFixed(2), debit: '', balance: this.balance});
     }
 
     makeWithdrawal(amount, date) {
         this.balance -= amount;
-        this.transactions.push( {date: date, credit: '', debit: amount, balance: this.balance});
+        this.transactions.push( {date: date, credit: '', debit: amount.toFixed(2), balance: this.balance});
     }
     checkTransactions() {
         return this.transactions;
     }
+
+    printStatement() {
+        console.log("date || credit || debit || balance");
+        this.transactions.forEach(transaction => {
+        console.log(`${transaction.date} || ${transaction.credit} || ${transaction.debit} || ${transaction.balance.toFixed(2)}`)
+    })
+}
 }
 
 module.exports = BankAccount;
+
+const account = new BankAccount();
+account.makeDeposit(1000, '10/01/2023');
+account.makeDeposit(2000, '13/01/2023');
+account.makeWithdrawal(500, '14/01/2023');
+account.printStatement();
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -19,8 +19,8 @@ describe("BankAccount", () => {
 
     it("date and amount of deposit recorded to the transactions", () => {
         expect(bankAccount.checkTransactions()).toEqual([
-            {date: '05/08/2023', credit: 1000, debit: '', balance: 1000},
-            {date: '12/08/2023', credit: 2000, debit: '', balance: 3000}
+            {date: '05/08/2023', credit: '1000.00', debit: '', balance: 1000},
+            {date: '12/08/2023', credit: '2000.00', debit: '', balance: 3000}
     ]);
     });
     
@@ -31,9 +31,25 @@ describe("BankAccount", () => {
 
     it("date and amount of withdrawal recorded to the transactions", () => {
         expect(bankAccount.checkTransactions()).toEqual([
-            {date: '05/08/2023', credit: 1000, debit: '', balance: 1000},
-            {date: '12/08/2023', credit: 2000, debit: '', balance: 3000},
-            {date: '15/08/2023', credit: '', debit: 500, balance: 2500}
-    ]);
+            {date: '05/08/2023', credit: '1000.00', debit: '', balance: 1000},
+            {date: '12/08/2023', credit: '2000.00', debit: '', balance: 3000},
+            {date: '15/08/2023', credit: '', debit: '500.00', balance: 2500}
+            ]);
     });
 });
+
+describe("Acceptance criteria", () => {
+    const bankAccount = new BankAccount();
+    
+    it("tests", () => {
+        expect(bankAccount.checkBalance()).toBe(0);
+        bankAccount.makeDeposit(1000, '05/08/2023');
+        bankAccount.makeDeposit(2000, '12/08/2023');
+        bankAccount.makeWithdrawal(500, '15/08/2023');
+        expect(bankAccount.checkTransactions()).toEqual([
+            {date: '05/08/2023', credit: '1000.00', debit: '', balance: 1000},
+            {date: '12/08/2023', credit: '2000.00', debit: '', balance: 3000},
+            {date: '15/08/2023', credit: '', debit: '500.00', balance: 2500}
+            ]);
+    })
+})
