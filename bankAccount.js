ffc13b2d594cc6de3605f1e3277617da13da5ce1
Denis <deniam24@gmail.com>
2023-09-05 22:19:56 +0100

    client can check transaction list and make a withdrawal

--- bankAccount.js
+++ bankAccount.js
@@ -1,13 +1,23 @@
 class BankAccount {
     constructor() {
         this.balance = 0;
+        this.transactions = [];
     }
     
     checkBalance() {
         return this.balance;
     }
+
     makeDeposit(amount, date) {
         this.balance += amount;
+        this.transactions.push( {date: date, credit: amount, debit: '', balance: this.balance});
+    }
+
+    makeWithdrawal(amount, date) {
+        this.balance-= amount;
+    }
+    checkTransactions() {
+        return this.transactions;
     }
 }
 
--- bankAccount.test.js
+++ bankAccount.test.js
@@ -11,4 +11,22 @@ describe("BankAccount", () => {
         bankAccount.makeDeposit(1000, '05/08/2023');
         expect(bankAccount.checkBalance()).toBe(1000);
     });
+
+    it("client makes a deposit of 2000 and balance increased on this value", () => {
+        bankAccount.makeDeposit(2000, '12/08/2023');
+        expect(bankAccount.checkBalance()).toBe(3000);
+    });
+
+    it("date and amount of deposit recorded to the transactions", () => {
+        expect(bankAccount.checkTransactions()).toEqual([
+            {date: '05/08/2023', credit: 1000, debit: '', balance: 1000},
+            {date: '12/08/2023', credit: 2000, debit: '', balance: 3000}
+    ]);
+    });
+    
+    it("client make a withdrawal of 500 and balance discreased on this value", () => {
+        bankAccount.makeWithdrawal(2000, '10/08/2023');
+        expect(bankAccount.checkBalance()).toBe(1000);
+    })
+
 });
